bddf37cb9d9621e13e7a96d7ff92e45c9ce6e3a0
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-02-23 00:09:17 +0900

    Create Member Service

---
+++ hello-spring/src/main/java/hyo/hellospring/service/MemberService.java
@@ -0,0 +1,37 @@
+package hyo.hellospring.service;
+
+import hyo.hellospring.domain.Member;
+import hyo.hellospring.repository.MemberRepository;
+import hyo.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복회원 검사
+        memberRepository.save(member);
+        return member.getId(); //id 반환
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> { //null이 아닌 값이 있으면 throw
+                    throw new IllegalStateException("이미 존재하는 이름의 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // id로 회원 한 명 조회
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
